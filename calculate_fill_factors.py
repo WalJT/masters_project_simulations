4e4cab309510a0835d8a46ee1a0eef00db09ea26
WalJT <waljt@pm.me>
2021-03-03 11:30:33 +0000

    Update calculate_fill_factors.py

--- calculate_fill_factors.py
+++ calculate_fill_factors.py
@@ -5,14 +5,13 @@ Script to calculate fill factors of various lattices
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 def area_circle(r: [int, float]) -> float:
     """
     Calculates the area of a circle
     :param r: Radius of the Circle
     :return: Area of the circle
     """
-    return np.pi * (r^2)
+    return np.pi * (r**2)
 
 
 # Start with the square lattice of circles
@@ -21,15 +20,19 @@ def square_circs():
 
     :return:
     """
-    radii = np.linspace(0, 1e4, 0.5)
+    lattice_vector_length = 1
+    square_area = lattice_vector_length**2
 
+    radii = np.linspace(0, 0.5, int(1e4)) # end of range is close packed
+
+    circle_areas, fill_factors = list(), list()
     i = 0
-    areas = np.array()
     for r in radii:
-        areas[i] = area_circle(r)
+        circle_areas.append(area_circle(r))
+        fill_factors.append(circle_areas[i])
         i += 1
 
-    plt.plot(radii, areas)
+    plt.plot(radii, fill_factors)
     plt.show()
 
 
