ff14d956c8c4f73edee1677a86f1d6adad5346f2
WalJT <waljt@pm.me>
2021-03-03 12:03:54 +0000

    Update calculate_fill_factors.py Circle packing fraction can be calculated

--- calculate_fill_factors.py
+++ calculate_fill_factors.py
@@ -5,35 +5,45 @@ Script to calculate fill factors of various lattices
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def area_circle(r: [int, float]) -> float:
     """
     Calculates the area of a circle
     :param r: Radius of the Circle
     :return: Area of the circle
     """
-    return np.pi * (r**2)
+    return np.pi * (r ** 2)
+
+
+def volume_sphere(r: [int, float]) -> float:
+    """
+    Calculates the volume of a sphere given the radius
+    :param r: radius
+    :return: volume
+    """
+    return (4/3) * np.pi * (r**3)
 
 
 # Start with the square lattice of circles
-def square_circs():
+def square_circs(r: [int, float], a: [int, float]) -> float:
+    """
+    Calculates the packing fraction of a square lattice of circles
+    This is applicable to infinitely long cylinders
+    :param r: radius of the circle
+    :param a: length of a the lattice vector
+    :return: Packing fraction (complete fill is 1)
     """
 
-    :return:
-    """
-    lattice_vector_length = 1
-    square_area = lattice_vector_length**2
+    if (r > a/2):
+        print("The packing fraction is greater than close packed")
+        # TODO: handle this case
 
-    radii = np.linspace(0, 0.5, int(1e4)) # end of range is close packed
+    square_area = a ** 2
+    circle_area = area_circle(r)
 
-    circle_areas, fill_factors = list(), list()
-    i = 0
-    for r in radii:
-        circle_areas.append(area_circle(r))
-        fill_factors.append(circle_areas[i])
-        i += 1
-
-    plt.plot(radii, fill_factors)
-    plt.show()
+    packing_fraction = circle_area/square_area
+    return packing_fraction
 
 
-square_circs()
+if __name__ == "__main__":
+    print(square_circs(0.5, 1))
