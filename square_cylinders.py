2ffe0a2237d348aa55b088d5db8fe8103a526012
WalJT <waljt@pm.me>
2021-03-06 09:21:18 +0000

    Update square_cylinders.py

--- square_cylinders.py
+++ square_cylinders.py
@@ -27,7 +27,7 @@ geometry_lattice = mp.Lattice(size=mp.Vector3(1, 1))
 
 # Create an array of cylinders with different radii
 
-resolution = 42  # Lattice constant is this many pixels
+resolution = 100  # Lattice constant is this many pixels
 
 radii = (0.1, 0.2, 0.3, 0.4, 0.5)  # Radii as a in units of a
 
@@ -63,9 +63,9 @@ for r in radii:
     # Scatter plot for multiple y values, see https://stackoverflow.com/a/34280815/2261298
     for xz, tmz, tez in zip(x, tm_freqs, te_freqs):
         ax.scatter([xz] * len(tmz), tmz, color='blue')
-        ax.scatter([xz] * len(tez), tez, color='red', facecolors='none')
+        # ax.scatter([xz] * len(tez), tez, color='red', facecolors='none')
     ax.plot(tm_freqs, color='blue')
-    ax.plot(te_freqs, color='red')
+    # ax.plot(te_freqs, color='red')
     ax.set_ylim([0, 1])
     ax.set_xlim([x[0], x[-1]])
 
@@ -80,7 +80,7 @@ for r in radii:
 
     # Plot labels
     ax.text(12, 0.04, 'TM bands', color='blue', size=15)
-    ax.text(13.05, 0.235, 'TE bands', color='red', size=15)
+    # ax.text(13.05, 0.235, 'TE bands', color='red', size=15)
 
     points_in_between = (len(tm_freqs) - 4) / 3
     tick_locs = [i * points_in_between + i for i in range(4)]
