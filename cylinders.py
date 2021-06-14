3ed6b74750b917a93b57d1657acf6636bf3f0bd2
WalJT <waljt@pm.me>
2021-06-14 16:49:18 +0100

    Added file for finite dimension structure

--- cylinders.py
+++ cylinders.py
@@ -115,7 +115,7 @@ if __name__ == "__main__":
     num_bands = 4
     radius = 0.32  # radius of the cylinders in terms of lattice constant
     rods_material = mp.air
-    bulk_material = materials.aSi
+    bulk_material = mp.Medium(index=3.42)
     geometry_lattice, geometry, k_points = set_up_crystal(radius, rods_material)
     k_points = mp.interpolate(40, k_points)
     resolution = 100  # Lattice constant is this many pixels
---
+++ triangular_lattice_meep.py
@@ -0,0 +1,7 @@
+"""
+A two dimensional lattice of infinitely long
+cylindrical rods. Triangular with finite dimensions
+"""
+
+import meep as mp
+
