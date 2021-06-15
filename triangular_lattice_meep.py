1f4506b3ddacefae239e11e649768edc7575d30e
WalJT <waljt@pm.me>
2021-06-15 09:07:09 +0100

    start finite structure

--- triangular_lattice_meep.py
+++ triangular_lattice_meep.py
@@ -4,4 +4,9 @@ cylindrical rods. Triangular with finite dimensions
 """
 
 import meep as mp
+from meep import materials
 
+# Create the block of dielectric material
+geometry = [mp.Block(mp.Vector3(10,10,mp.inf),
+                     center=mp.Vector3(),
+                     material=mp.Medium(epsilon=12))]
