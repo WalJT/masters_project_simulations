c00b9d22d6988ff5c54fcbeed739982f4f8f0e8b
WalJT <waljt@pm.me>
2021-06-24 17:43:44 +0100

    implemented square lattice

--- finite_dimension_cylinders.py
+++ finite_dimension_cylinders.py
@@ -16,8 +16,8 @@ cylinder_material = mp.air
 waveguide_material = block_material
 
 # Create the block of dielectric material
-block_x_width = 1000
-block_y_width = 200
+block_x_width = 2000
+block_y_width = 500
 # Create a "Cell", the region in space
 cell = mp.Vector3(block_x_width + 200, block_y_width + 100, 0)
 geometry = [mp.Block(mp.Vector3(block_x_width, block_y_width, mp.inf, ),
@@ -28,25 +28,21 @@ geometry = [mp.Block(mp.Vector3(block_x_width, block_y_width, mp.inf, ),
 cylinder_radius = 21.0
 lattice_constant = 65.7
 starting_corner = mp.Vector3(-(block_x_width / 2) + cylinder_radius, -(block_y_width / 2) + cylinder_radius)
-points = [starting_corner]
-number_of_cols = int(block_x_width / lattice_constant) + 2
-number_of_rows = int(block_y_width / lattice_constant) + 20
+number_of_cols = int(block_x_width / lattice_constant)
+number_of_rows = int(block_y_width / lattice_constant)
 
 # Create a triangular lattice
-for point in lattices.triangular(lattice_constant, number_of_rows, number_of_cols, starting_corner):
+for point in lattices.square(lattice_constant, number_of_rows, number_of_cols, starting_corner):
     geometry.append(mp.Cylinder(radius=cylinder_radius, material=cylinder_material, center=point))
-
-# Hollow structure center to place a source
-# geometry.append(mp.Cylinder(radius=1, material=mp.air, center=mp.Vector3(0, 0)))
 
 # Place a source
 # use a gaussian source and get a transmission spectrum (https://meep.readthedocs.io/en/latest/Python_Tutorials/Resonant_Modes_and_Transmission_in_a_Waveguide_Cavity/)
 fcen = 1/300  # Center frequency
 df = 1/100    # pulse frequency width
-sources = [mp.Source(mp.GaussianSource(fcen, fwidth=df),  # 1/wavelength in microns
-                     component=mp.Ey,
-                     size=mp.Vector3(0, 20),
-                     center=mp.Vector3((block_x_width + 100) / 2, 0, 0))]
+sources = [mp.Source(mp.ContinuousSource(fcen),  # 1/wavelength in microns
+                     component=mp.Ez,
+                     size=mp.Vector3(0, 0),
+                     center=mp.Vector3((block_x_width+100) / 2, 0, 0))]
 
 # Add a waveguide
 # wg1 = mp.Block(mp.Vector3(block_width/2 - 1, 1.2, mp.inf),
@@ -79,20 +75,20 @@ flux_plane = mp.Vector3(-(block_x_width/2 + 50))
 freg = mp.FluxRegion(center=flux_plane,
                      size=mp.Vector3(0, 40))
 
-nfreq = 500  # number of frequencies at which to compute flux
+# nfreq = 500  # number of frequencies at which to compute flux
 
 # transmitted flux
-trans = sim.add_flux(fcen, df, nfreq, freg)
+# trans = sim.add_flux(fcen, df, nfreq, freg)
 
 # Run the simulation
-# sim.run(mp.at_beginning(mp.output_epsilon), mp.to_appended("Ey", mp.at_every(1, mp.output_hfield_z)),  until=5000)
-# sim.run(until_after_sources=mp.stop_when_fields_decayed(50, mp.Ey, flux_plane, 1e-3))
+# sim.run(mp.at_beginning(mp.output_epsilon), mp.to_appended("ez", mp.at_every(1, mp.output_efield_z)),  until=5000)
+# sim.run(until_after_sources=mp.stop_when_fields_decayed(50, mp.Ez, flux_plane, 1e-3))
 sim.run(until=5000)
-sim.display_fluxes(trans)
-transmitted_flux = mp.get_fluxes(trans)
-flux_freqs = mp.get_flux_freqs(trans)
-plt.plot(flux_freqs, transmitted_flux)
-plt.show()
+# sim.display_fluxes(trans)
+# transmitted_flux = mp.get_fluxes(trans)
+# flux_freqs = mp.get_flux_freqs(trans)
+# plt.plot(flux_freqs, transmitted_flux)
+# plt.show()
 
 # plot data using matplotlib
 # First the dielectric
@@ -103,9 +99,9 @@ plt.axis('off')
 plt.show()
 
 # and then the field diagram
-hz_data = sim.get_array(center=mp.Vector3(), size=cell, component=mp.Hz)
+ez_data = sim.get_array(center=mp.Vector3(), size=cell, component=mp.Ez)
 plt.figure()
 plt.imshow(eps_data.transpose(), interpolation='spline36', cmap='binary')
-plt.imshow(hz_data.transpose(), interpolation='spline36', cmap='RdBu', alpha=0.9)
+plt.imshow(ez_data.transpose(), interpolation='spline36', cmap='RdBu', alpha=0.9)
 plt.axis('off')
 plt.show()
--- lattices.py
+++ lattices.py
@@ -16,6 +16,7 @@ def triangular(lattice_constant: float, number_of_rows: int, number_of_cols: int
     lattice_vectors = (mp.Vector3(lattice_constant, 0),
                        mp.Vector3(lattice_constant / 2, np.sqrt(3) * lattice_constant / 2),
                        mp.Vector3(-lattice_constant / 2, np.sqrt(3) * lattice_constant / 2))
+
     for col in range(number_of_cols):
         for row in range(number_of_rows):
             if row == 0:
@@ -33,5 +34,20 @@ def triangular(lattice_constant: float, number_of_rows: int, number_of_cols: int
     return lattice_sites
 
 
-def square():
-    pass  # TODO
+def square(lattice_constant: float, number_of_rows: int, number_of_cols: int, starting_point: mp.Vector3) -> list:
+    lattice_sites = [starting_point]
+    lattice_vectors = (mp.Vector3(lattice_constant, 0),
+                       mp.Vector3(0, lattice_constant))
+
+    for col in range(number_of_cols):
+        for row in range(number_of_rows):
+            if row == 0:
+                new_point = lattice_sites[row] + col * (lattice_vectors[0])
+            else:
+                new_point = lattice_sites[row] + (lattice_vectors[1]) + col * (lattice_vectors[0])
+            lattice_sites.append(new_point)
+
+    new_point = lattice_sites[col] + (col * lattice_vectors[0]) - (col * lattice_vectors[1])
+    lattice_sites.append(new_point)
+
+    return lattice_sites
