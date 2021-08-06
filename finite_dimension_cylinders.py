76752b4655b3ef8cd1de394a986934e6eca5f518
WalJT <waljt@pm.me>
2021-08-06 16:24:58 +0100

    narrower pulse

--- finite_dimension_cylinders.py
+++ finite_dimension_cylinders.py
@@ -19,7 +19,7 @@ lattice_constant = 1
 pml_thickness = 3*lattice_constant
 cylinder_radius = 0.32*lattice_constant
 block_x_width = np.ceil(15*lattice_constant)
-block_y_width = np.ceil(20*lattice_constant)
+block_y_width = np.ceil(30*lattice_constant)
 resolution = 40  # Resolution in pixels per micron
 polarization = "te"  # "tm" or "te"
 
@@ -34,11 +34,11 @@ else:
     sys.exit(1)
 
 # Current source information
-fcen = 0.6  # (Center) frequency; 1/wavelength in microns
-df = 0.59  # pulse frequency width (for Gaussian Sources)
+fcen = 1/1.6  # (Center) frequency; 1/wavelength in microns
+df = 0.3  # pulse frequency width (for Gaussian Sources)
 source_x_loc = -(block_x_width/2 + 2*lattice_constant)
 source_y_loc = 0
-source_size = mp.Vector3(0, 5*lattice_constant)
+source_size = mp.Vector3(0, 2.5*lattice_constant)
 
 # Create the block of dielectric material
 # Create a "Cell", the region in space
@@ -52,11 +52,11 @@ geometry = [mp.Block(mp.Vector3(block_x_width, block_y_width, mp.inf),
 
 starting_corner = mp.Vector3(-(block_x_width / 2) + cylinder_radius, -(block_y_width / 2) + cylinder_radius)
 number_of_cols = int(block_x_width / lattice_constant)
-number_of_rows = int(block_y_width / lattice_constant)+4
+number_of_rows = int(block_y_width / lattice_constant)+6
 
 # Create a square lattice
-# for point in lattices.triangular(lattice_constant, number_of_rows, number_of_cols, starting_corner):
-#     geometry.append(mp.Cylinder(radius=cylinder_radius, material=cylinder_material, center=point))
+for point in lattices.triangular(lattice_constant, number_of_rows, number_of_cols, starting_corner):
+    geometry.append(mp.Cylinder(radius=cylinder_radius, material=cylinder_material, center=point))
 
 # Place a source use a gaussian source and get a transmission spectrum
 # (https://meep.readthedocs.io/en/latest/Python_Tutorials/Resonant_Modes_and_Transmission_in_a_Waveguide_Cavity/)
@@ -67,9 +67,9 @@ sources = [mp.Source(mp.GaussianSource(fcen, fwidth=df),
                      center=mp.Vector3(source_x_loc, source_y_loc, 0))]
 
 # Add a waveguide
-# wg1 = mp.Block(mp.Vector3(block_x_width+3, lattice_constant*1.5, mp.inf),
-#                center=mp.Vector3(0, 0),
-#                material=waveguide_material)
+wg1 = mp.Block(mp.Vector3(block_x_width, 0.3*lattice_constant, mp.inf),
+               center=mp.Vector3(0-0.5*lattice_constant, 0),
+               material=waveguide_material)
 # wg1 = mp.Block(mp.Vector3(block_width/4, 1.2, mp.inf),
 #                center=mp.Vector3(block_width/8 + 1, 0),
 #                material=waveguide_material)
@@ -77,7 +77,7 @@ sources = [mp.Source(mp.GaussianSource(fcen, fwidth=df),
 #                center=mp.Vector3(block_width/4 + 0.5, block_width/4),
 #                material=waveguide_material)
 #
-# geometry.append(wg1)
+geometry.append(wg1)
 # geometry.append(wg2)
 
 # "Perfectly Matched Layers" (cell boundaries)
@@ -100,9 +100,9 @@ nfreq = 500  # number of frequencies at which to compute flux
 trans = sim.add_flux(fcen, df, nfreq, freg)
 
 # Run the simulation
-# sim.run(mp.at_beginning(mp.output_epsilon), mp.to_appended("ez", mp.at_every(0.1, mp.output_bfield_z)),  until=100)
+# sim.run(mp.at_beginning(mp.output_epsilon), mp.to_appended("ez", mp.at_every(0.1, mp.output_efield_z)),  until=100)
 sim.run(until_after_sources=mp.stop_when_fields_decayed(50, plot_component, flux_plane, 1e-3))
-# sim.run(until=100)
+# sim.run(until=1)
 # sim.display_fluxes(trans)
 
 # Get the frequencies and flux values
